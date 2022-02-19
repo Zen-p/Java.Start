215fdd52007796bc1ba6ca1d45af73398168bf50
Evgeni2 <98706643+Evgeni2@users.noreply.github.com>
2022-02-19 16:11:47 +0300

    tt

--- week_3/Task1.java
+++ week_3/Task1.java
@@ -1,9 +1,27 @@
 package week_3;
 
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Task1 {
-    String s = "Hello";
-    String s1 = s.toLowerCase();
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите 5 чисел:");
+        int[] nums = new int[5];
+        nums[0] = s.nextInt();
+        nums[1] = s.nextInt();
+        nums[2] = s.nextInt();
+        nums[3] = s.nextInt();
+        nums[4] = s.nextInt();
 
+        for (int i = 0; i < 5; i++) {
+            if (nums[i] % 2 == 0) {
+                System.out.println("Число x - четное");
+            } else {
+                System.out.println("Число x - нечетное");
+            }
+        }
+
+    }
 }
+
+
---
+++ week_3/Task2.java
@@ -0,0 +1,26 @@
+package week_3;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char [] symbol = text.toCharArray();
+        String newText = sc.nextLine();
+        char[] shifr = newText.toCharArray();
+        System.out.println("Введите число:");
+        int x = sc.nextInt();
+
+
+        for (int i = 0; i < shifr.length; i++) {
+            System.out.print(shifr[i+x]);
+        }
+
+
+
+
+
+    }
+}
