b2424ff8f9feac185fe70a6ed226136222ac3ae1
Evgeni2 <98706643+Evgeni2@users.noreply.github.com>
2022-02-20 16:41:05 +0300

    tt

--- week_3/Task2.java
+++ week_3/Task2.java
@@ -1,26 +1,25 @@
 package week_3;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите текст");
-        String text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        char [] symbol = text.toCharArray();
-        String newText = sc.nextLine();
-        char[] shifr = newText.toCharArray();
-        System.out.println("Введите число:");
-        int x = sc.nextInt();
+        System.out.println("Введите строку");
+        String rootString = sc.nextLine();
+        System.out.println("Введите число");
+        int encryprion = sc.nextInt();
+        char [] mas = rootString.toCharArray();
+        int len = mas.length;
+        char [] newtext = new char[len];
+        for (int i = 0; i < len; i++) {
+            int a = (int) mas[i] + encryprion;
+            newtext[i] = (char) a;
 
-
-        for (int i = 0; i < shifr.length; i++) {
-            System.out.print(shifr[i+x]);
-        }//Ума не приложу что не так
-
-
-
-
+        }
+        String  encryptedString = new String(newtext);
+        System.out.println(encryptedString);
 
     }
 }
