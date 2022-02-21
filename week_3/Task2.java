5ec1772f6e981e4448c2e5f1d9080473b50e4d3f
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-02-21 17:25:17 +0400

    Feedback

--- week_3/Task2.java
+++ week_3/Task2.java
@@ -3,21 +3,26 @@ package week_3;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
+// тут тоже все хорошо! единственное, я добавил переносы строк, разделив код на логические абзацы, так читается лучше
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Введите строку");
         String rootString = sc.nextLine();
         System.out.println("Введите число");
         int encryprion = sc.nextInt();
+        
         char [] mas = rootString.toCharArray();
         int len = mas.length;
         char [] newtext = new char[len];
+        
         for (int i = 0; i < len; i++) {
             int a = (int) mas[i] + encryprion;
             newtext[i] = (char) a;
 
         }
+        
         String  encryptedString = new String(newtext);
         System.out.println(encryptedString);
 
