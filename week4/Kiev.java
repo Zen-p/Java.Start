ca90a3e5fa7603e8d03cdbdc662a4dc9bdcc2d11
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-02-27 12:04:03 +0400

    Feedback

--- week4/Kiev.java
+++ week4/Kiev.java
@@ -3,7 +3,10 @@ package week4;
 import static java.lang.Thread.*;
 
 public class Kiev {
+    //не хватает правильно расставленных модификаторов доступа. В данном случае этот метод будет public
+    //названия мтодов должны быть глаголами и обозначать какое-то действие. latte -> makeLatte
     void latte() {
+        // вот здесь должен вызываться метод waiting
         System.out.println("Thanks for order in Kiev, take your latte. Bye!");
     }
     void espresso() {
@@ -15,6 +18,7 @@ public class Kiev {
     void coffee_with_milk() {
         System.out.println("Thanks for order in Kiev, take your coffee with milk. Bye!");
     }
+    //так как все вызовы этого метода будут внутри этого класса то можно сделать его private
     void waiting (){
         System.out.println("Wait a few seconds:");
     }
