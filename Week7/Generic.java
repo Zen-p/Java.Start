1a796b283c7e775a11ee44789dfc2469efe7a7c8
Evgeni2 <98706643+Evgeni2@users.noreply.github.com>
2022-03-20 17:01:25 +0300

    Update Generic.java

--- Week7/Generic.java
+++ Week7/Generic.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Generic {
     public static void main(String[] args) {
-        //Попытка создания чего-то похожего не дженерик
+        //Попытка создания чего-то похожего нa дженерик
         ArrayList <Integer> newList = new ArrayList<>();//Integer - класс-обертка
         newList.add(1);
         newList.add(14);
