7f4673b314d239ad023feab6cdbdf75371826688
Evgeni2 <98706643+Evgeni2@users.noreply.github.com>
2022-03-31 18:36:25 +0300

    а

---
+++ Week7/BankMenu.java
@@ -0,0 +1,40 @@
+package Week7;
+
+import java.util.Scanner;
+
+public class BankMenu {
+    private Bank bank;
+
+    public BankMenu(Bank bank) {
+        this.bank = bank;
+    }
+
+    public void showStartMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите операцию:\n1.Вход\n2.Регистрация");
+
+        int logOrReg = sc.nextInt();
+        if (logOrReg == 1) {
+            showLogin();
+        } else if (logOrReg == 2) {
+            showRegister();
+        } else {
+            System.out.println("Выбрана неверная операция!");
+        }
+    }
+
+        public void showBankMenu() {
+            System.out.println("1. Show my info\n2. Add loan\n3. Add debit card\n0. Exit");
+        }
+
+    private void showLogin(){
+
+        }
+
+    private void showRegister(){
+
+        }
+
+
+    }
+
--- Week7/Main.java
+++ Week7/Main.java
@@ -1,8 +1,10 @@
 package Week7;
 
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Bank bank = new Bank();
         bank.start();
     }
